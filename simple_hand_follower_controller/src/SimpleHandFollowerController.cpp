fb089b208e7fe2d4c5436fb7f86bc82a99342508
david_vl2003@hotmail.com <david_vl2003@hotmail.com>
2025-04-24 19:16:24 -0600

    Addition of simple hand controllers

--- simple_hand_follower_controller/src/SimpleHandFollowerController.cpp
+++ simple_hand_follower_controller/src/SimpleHandFollowerController.cpp
@@ -1,85 +1,56 @@
-#include "SimpleHandFollowerController.h"
+#pragma once
 
-SimpleHandFollowerController::SimpleHandFollowerController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
-: mc_control::MCController(rm, dt)
+#include <mc_control/mc_controller.h>
+#include <mc_tasks/CoMTask.h>
+#include <mc_tasks/EndEffectorTask.h>
+#include <mc_tasks/MetaTaskLoader.h>
+
+#include "api.h"
+
+struct SimpleHandFollowerController_DLLAPI SimpleHandFollowerController : public mc_control::MCController 
 {
-  config_.load(config);
-  comTask = std::make_shared<mc_tasks::CoMTask>(robots(), 0, 10.0, 1000.0);
-  eflTask = std::make_shared<mc_tasks::EndEffectorTask>("l_wrist", robots(), 0, 5.0, 500.0);
-  efrTask = std::make_shared<mc_tasks::EndEffectorTask>("r_wrist", robots(), 0, 5.0, 500.0);
+  SimpleHandFollowerController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration &config);
 
-  solver().addConstraintSet(contactConstraint);
-  solver().addConstraintSet(dynamicsConstraint);
-  solver().addTask(postureTask);
-  solver().addTask(eflTask);
-  solver().addTask(efrTask);
+  bool run() override;
 
-  addContact({robot().name(), "ground", "LeftFoot", "AllGround"});
-  addContact({robot().name(), "ground", "RightFoot", "AllGround"});
-  solver().addTask(comTask);
-  postureTask->stiffness(1);
-  jointIndex = robot().jointIndexByName("NECK_Y");
+  void reset(const mc_control::ControllerResetData &reset_data) override;
+
+  void switch_target();
+
+  void switch_com_target();
+
+  void move_hand(Eigen::Vector3d, Eigen::Quaterniond, bool);
+
+  void return_left_hand();
+
+private:
+  mc_rtc::Configuration config_;
   
-  mc_rtc::log::success("SimpleHandFollowerController init done ");
-}
-
-bool SimpleHandFollowerController::run()
-{
-  // if(comTask->eval().norm() < 0.01)
-  // {
-  //   switch_com_target();
-  // }
-
-  move_left_hand();
-
-  if(std::abs(postureTask->posture()[jointIndex][0] - robot().mbc().q[jointIndex][0]) < 0.05)
-  {
-    switch_target();
-  }
+  int jointIndex = 0;
+  int state = 0;
+  bool goingLeft = true;
+  bool handAtTarget = true;
+  bool comDown = false;
+  bool isLeftHand = false;
+  bool isRightHand = false;
+  bool bothHands = false;
+  bool leftHandAtTarget = true;
+  bool rightHandAtTarget = true;
+  bool leftWasAtTarget = false;
+  bool rightWasAtTarget = false;
+  bool bothWereAtTarget = false;
   
-  return mc_control::MCController::run();
-}
+  std::shared_ptr<mc_tasks::CoMTask> comTask;
+  std::shared_ptr<mc_tasks::EndEffectorTask> efrTask;
+  std::shared_ptr<mc_tasks::EndEffectorTask> eflTask;
 
-void SimpleHandFollowerController::reset(const mc_control::ControllerResetData & reset_data)
-{
-  mc_control::MCController::reset(reset_data);
-  eflTask->reset();
-  efrTask->reset();
-  comTask->reset();
-  comZero = comTask->com();
-}
-
-void SimpleHandFollowerController::switch_com_target()
-{
-  // comZero is obtained by doing:
-  // comZero = comTask->com();
-  // in the reset function
-  if(comDown) { comTask->com(comZero - Eigen::Vector3d{0, 0, 0.2}); }
-  else { comTask->com(comZero); }
-  comDown = !comDown;
-}
-
-void SimpleHandFollowerController::move_left_hand()
-{
-  
-  auto pt = eflTask->get_ef_pose();
-  eflTask->set_ef_pose(sva::PTransformd{sva::RotY(-M_PI / 2), Eigen::Vector3d{0.5, 0.25, 1.1}});
-
-
-}
-
-
-void SimpleHandFollowerController::switch_target()
-{
-  if(goingLeft)
-  {
-    postureTask->target({{"NECK_Y", robot().qu()[jointIndex]}});
-  }
-  else
-  {
-    postureTask->target({{"NECK_Y", robot().ql()[jointIndex]}});
-  }
-  goingLeft = !goingLeft;
-}
-
-CONTROLLER_CONSTRUCTOR("SimpleHandFollowerController", SimpleHandFollowerController)
+  Eigen::Vector3d comZero;
+  Eigen::Vector3d left_hand_initial_position;
+  Eigen::Vector3d left_hand_target_position;
+  Eigen::Vector3d right_hand_initial_position;
+  Eigen::Vector3d right_hand_target_position;
+  Eigen::Quaterniond left_hand_initial_quaternion;
+  Eigen::Quaterniond left_hand_target_quaternion;
+  Eigen::Quaterniond right_hand_initial_quaternion;
+  Eigen::Quaterniond right_hand_target_quaternion;
+};
